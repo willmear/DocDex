ea25ce71818f90e4c8cd56015549dee6640226d9
Will Mear <wxm158@student.bham.ac.uk>
2025-02-11 17:10:42 +0000

    read pdf

---
+++ backend/src/main/java/com/willmear/DocDex/CommandLineAppStartupRunner.java
@@ -0,0 +1,17 @@
+package com.willmear.DocDex;
+
+import com.willmear.DocDex.service.PdfService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLineAppStartupRunner implements CommandLineRunner {
+    @Autowired
+    private PdfService pdfService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        pdfService.readPDF();
+    }
+}
---
+++ backend/src/main/java/com/willmear/DocDex/service/PdfService.java
@@ -0,0 +1,49 @@
+package com.willmear.DocDex.service;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.multipdf.Splitter;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.ParagraphPdfDocumentReader;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PdfService {
+
+
+    public static List<Document> readPDF() throws IOException {
+
+            File file = new File("C:/Users/willi/Desktop/DocDex/spring-boot-reference.pdf");
+            PDDocument document = Loader.loadPDF(file);
+            Splitter splitter = new Splitter();
+            splitter.setSplitAtPage(1);
+            List<PDDocument> documents = splitter.split(document);
+            PDFTextStripper stripper = new PDFTextStripper();
+            System.out.println("READ\n\n");
+            String text = stripper.getText(document);
+            System.out.println(stripper.getText(documents.get(0)));
+            System.out.println("\n\n\n\n\n\n\n");
+            System.out.println(stripper.getText(documents.get(1)));
+
+            List<Document> documentList = new ArrayList<>();
+
+            for (PDDocument document1 : documents) {
+                Document document2 = new Document(stripper.getText(document1));
+                documentList.add(document2);
+            }
+
+
+        return documentList;
+    }
+
+}
