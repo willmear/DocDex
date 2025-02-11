f21ce468ac4b08954cda8f02a49ced3ca78b3dc3
Will Mear <wxm158@student.bham.ac.uk>
2025-02-11 14:57:13 +0000

    initial commit

---
+++ backend/src/main/java/com/willmear/DocDex/DocDexApplication.java
@@ -0,0 +1,13 @@
+package com.willmear.DocDex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocDexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocDexApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/willmear/DocDex/DocDexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willmear.DocDex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocDexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
